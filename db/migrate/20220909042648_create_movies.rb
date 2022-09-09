2fd34da7a4004939a330468ac8fc4221c2df3862
Andrew Walters <andrewjpwalters@gmail.com>
2022-09-08 23:40:50 -0500

    update content of initial migrations

--- db/migrate/20220909042648_create_movies.rb
+++ db/migrate/20220909042648_create_movies.rb
@@ -1,4 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[6.1]
   def change
+    create_table :movies do |t|
+      t.string :name
+      t.string :genre
+      t.integer :year
+      t.string :comment
+    end
   end
 end
